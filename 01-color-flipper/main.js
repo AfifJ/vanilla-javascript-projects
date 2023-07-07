13694237a6714182853dc604656f6a0d3a47a859
AfifJ <110212622+AfifJ@users.noreply.github.com>
2023-07-07 17:27:40 +0700

    :rocket: : Make simple and hex color

--- 01-color-flipper/main.js
+++ 01-color-flipper/main.js
@@ -12,17 +12,44 @@ const simpleColor = async () => {
     return (err);
   }
 }
-let color = [];
-(
-  async () => {
-    color = await simpleColor();
-  }
-)()
 
+const pickSimpleColor = async () => {
+  const color = (await simpleColor()).color;
+  const colorId = Math.ceil(Math.random() * color.length) - 1;
+  return color[colorId]
+}
 
-flipColor.addEventListener("click", () => {
-  const colorId = Math.ceil(Math.random() * 147) - 1;
-  const colorPicked = color.color[colorId]
+const getHex = () => {
+  const eHex = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'
+  ]
+  const randomEHex = () => eHex[Math.ceil(Math.random() * eHex.length) - 1];
+  const totalE = () => Math.ceil(Math.random() * 3) + 3;
+
+  const hex = [...Array(totalE())].map(() => randomEHex()).join('').toString();
+  return hex;
+}
+
+const changeBg = async () => {
+  getHex();
+  let filename = String(document.location.pathname);
+  filename = filename.substring(filename.lastIndexOf('/') + 1);
+
+  let colorPicked = filename == 'index.html' ? await pickSimpleColor() :
+    filename == 'hex.html' && '#' + getHex();
+
   colorArea.style.background = colorPicked;
   colorDesc.innerHTML = `Background Color : <br> ${colorPicked}`;
+}
+/* 
+to remove the auto change backgroud,
+just delete all interval and autochange
+*/
+let autoChange;
+flipColor.addEventListener("click", () => {
+  changeBg();
+  clearInterval(autoChange);
+});
+window.addEventListener("load", () => {
+  autoChange = setInterval(changeBg, 3000);
 });
